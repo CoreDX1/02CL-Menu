41ba808c3cdf55e9bc8dc51218d24bbe75169503
CoreDX1 <chrismaquis@gmail.com>
2022-07-14 09:59:17 -0300

    Menu

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,59 @@
+import './NavBar.scss';
+
+interface Menu {
+  name: string;
+  link: string;
+}
+
+const NavBar = () => {
+  const listMenu: Menu[] = [
+    {
+      name: "Monster",
+      link: "#moster",
+    },
+    {
+      name: "Weapons",
+      link: "#weapons",
+    },
+    {
+      name: "Armor",
+      link: "#armor",
+    },
+    {
+      name: "Classes",
+      link: "#classes",
+    },
+    {
+      name : 'Events',
+      link : '#events',
+    }
+  ];
+
+  const renderMenu = (): JSX.Element[] => {
+    return listMenu.map((menu: Menu) => {
+      return (
+        <li key={menu.name}>
+          <a href={menu.link}>{menu.name}</a>
+        </li>
+      );
+    });
+  };
+
+  return (
+    <>
+      <section className="container">
+        <header className="header">
+          <nav className="navbar">
+            <h1 className="title">Wikipedia</h1>
+            <ul className="nav">
+              <li><a className='active' href="/">Home</a></li>
+              {renderMenu()}
+              </ul>
+          </nav>
+        </header>
+      </section>
+    </>
+  );
+};
+
+export default NavBar;
